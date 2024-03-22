9f2c27221179b48421ed30c3f046a6ec8ba443c1
caoxuannghiem <nghiemcaoxuan0502@gmail.com>
2024-03-22 23:03:56 +0700

    Fast food store application

--- app/src/main/java/com/example/applicationfastfood/Activity/ListFoodsActivity.java
+++ app/src/main/java/com/example/applicationfastfood/Activity/ListFoodsActivity.java
@@ -1,18 +1,29 @@
 package com.example.applicationfastfood.Activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.applicationfastfood.Domain.Foods;
 import com.example.applicationfastfood.R;
 import com.example.applicationfastfood.databinding.ActivityListFoodsBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class ListFoodsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ListFoodsActivity extends BaseActivity {
 
     ActivityListFoodsBinding binding;
 private RecyclerView.Adapter adapterListFood;
@@ -27,13 +38,62 @@ private String searchText;
         binding=ActivityListFoodsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         getIntentExtra();
+        initList();
+
     }
 
+
+    private void initList()
+    {
+        DatabaseReference myRef=database.getReference("Foods");
+        binding.progressBar.setVisibility(View.VISIBLE);
+        ArrayList<Foods>list=new ArrayList<>();
+        Query query;
+        if(isSearch)
+        {
+            query = myRef.orderByChild("Title").startAt(searchText).endAt(searchText+'\uf8ff');
+        }
+        else
+        {
+            query = myRef.orderByChild("CategoryId").equalTo(categoryId);
+        }
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    for(DataSnapshot issue :snapshot.getChildren())
+                    {
+                        list.add(issue.getValue(Foods.class));
+                    }
+                    if(list.size()>0)
+                    {
+                        binding.foodListView.setLayoutManager(new GridLayoutManager(ListFoodsActivity.this,2));
+                        adapterListFood=new
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
     private void  getIntentExtra()
     {
         categoryId = getIntent().getIntExtra("CategoryId",0);
         categoryName=getIntent().getStringExtra("Categoty");
         searchText=getIntent().getStringExtra("text");
         isSearch=getIntent().getBooleanExtra("isSearch",false);
+
+        binding.titleTxt.setText(categoryName);
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
