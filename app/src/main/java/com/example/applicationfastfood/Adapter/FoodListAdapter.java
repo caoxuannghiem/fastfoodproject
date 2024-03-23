1b6aca1f229235902144112a05ac187433ef8ce9
caoxuannghiem <nghiemcaoxuan0502@gmail.com>
2024-03-23 21:09:26 +0700

    Fast food store application

---
+++ app/src/main/java/com/example/applicationfastfood/Adapter/FoodListAdapter.java
@@ -0,0 +1,27 @@
+package com.example.applicationfastfood.Adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewholder> {
+    @NonNull
+    @Override
+    public FoodListAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FoodListAdapter.viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder extends {
+    }
+}
