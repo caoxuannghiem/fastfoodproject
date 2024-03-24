22844d97b42dd25734d8d442d90070d0d1926048
caoxuannghiem <nghiemcaoxuan0502@gmail.com>
2024-03-24 18:25:50 +0700

    Fast food store application

--- app/src/main/java/com/example/applicationfastfood/Adapter/FoodListAdapter.java
+++ app/src/main/java/com/example/applicationfastfood/Adapter/FoodListAdapter.java
@@ -1,14 +1,31 @@
 package com.example.applicationfastfood.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
-
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.applicationfastfood.Domain.Foods;
+
+import java.util.ArrayList;
+
 public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewholder> {
+
+    ArrayList<Foods> items;
+    Context context;
+
+
+    public FoodListAdapter(ArrayList<Foods> items)
+    {
+        this.items = items;
+    }
     @NonNull
     @Override
     public FoodListAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context=parent.getContext();
+        View inflate = LayoutInflater.from(context).inflate(R.layout)
         return null;
     }
 
@@ -22,6 +39,11 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewho
         return 0;
     }
 
-    public class viewholder extends {
+    public class viewholder extends RecyclerView.ViewHolder{
+        public viewholder (@NonNull View itemView)
+        {
+            super (itemView);
+
+        }
     }
 }
