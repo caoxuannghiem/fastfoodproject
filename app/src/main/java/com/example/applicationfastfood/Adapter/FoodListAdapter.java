c14325f9b6df4ede303804124a7c095fbbf10684
caoxuannghiem <nghiemcaoxuan0502@gmail.com>
2024-03-25 22:03:37 +0700

    Fast food store application

--- app/src/main/java/com/example/applicationfastfood/Adapter/FoodListAdapter.java
+++ app/src/main/java/com/example/applicationfastfood/Adapter/FoodListAdapter.java
@@ -1,13 +1,18 @@
 package com.example.applicationfastfood.Adapter;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.applicationfastfood.Domain.Foods;
+import com.example.applicationfastfood.R;
 
 import java.util.ArrayList;
 
@@ -26,11 +31,15 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewho
     public FoodListAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context=parent.getContext();
         View inflate = LayoutInflater.from(context).inflate(R.layout.viewholder_list_food, parent,false);
-        return null;
+        return new viewholder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull FoodListAdapter.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.timeTxt.setText(items.get(position).getTimeValue()+"min");
+        holder.priceTxt.setText("$"+items.get(position).getPrice());
+
 
     }
 
@@ -40,10 +49,16 @@ public class FoodListAdapter extends RecyclerView.Adapter<FoodListAdapter.viewho
     }
 
     public class viewholder extends RecyclerView.ViewHolder{
+        TextView titleTxt, priceTxt, starTxt, timeTxt;
+        ImageView pic;
         public viewholder (@NonNull View itemView)
         {
             super (itemView);
-
+            titleTxt=itemView.findViewById(R.id.titleTxt);
+            priceTxt=itemView.findViewById(R.id.priceTxt);
+            starTxt=itemView.findViewById(R.id.starTxt);
+            timeTxt=itemView.findViewById(R.id.timeTxt);
+            pic=itemView.findViewById(R.id.pic);
         }
     }
 }
